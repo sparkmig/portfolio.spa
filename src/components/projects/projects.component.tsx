ec6f377c2ef7ebf0ff838d217d09433f6ee6fa04
Mikkel Gundersen <301mikkel103@email.com>
2024-07-28 23:04:56 +0200

    redesign

--- src/components/projects/projects.component.tsx
+++ src/components/projects/projects.component.tsx
@@ -5,56 +5,44 @@ export default function Projects() {
         <div className="flex flex-col lg:flex-row justify-between">
           <div className="mb-10 lg:mb-0">
             <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">
-              Portfolio
+              Projekter
             </h1>
             <p className="font-normal text-gray-500 text-xs md:text-base">
-              I have brought here my biggest and favorite works <br /> as a
+              I have brought here my biggest and favorite works as a
               professional.
             </p>
           </div>
-          <div className="space-y-24">
-            <div className="flex space-x-6">
-              <h1 className="font-normal text-gray-700 text-3xl md:text-4xl">
-                01
-              </h1>
-              <span className="w-28 h-0.5 bg-gray-300 mt-5"></span>
+          <div className="space-y-6 lg:w-2xl">
+            <div className="flex space-x-6 bg-gray-50 p-10 rounded-md ">
               <div>
-                <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">
+                <h1 className="font-semibold text-gray-700 text-xl md:text-2xl mb-5">
                   Demo API Generator
                 </h1>
-                <p className="font-normal text-gray-500 text-sm md:text-base">
+                <p className="font-normal text-gray-500 ">
                   A dummy data free and documented API generator to facilitate{" "}
                   <br /> the process of testing the front-end portion of
                   projects.
                 </p>
               </div>
             </div>
-            <div className="flex space-x-6">
-              <h1 className="font-normal text-gray-700 text-3xl md:text-4xl">
-                02
-              </h1>
-              <span className="w-28 h-0.5 bg-gray-300 mt-5"></span>
+            <div className="flex space-x-6 bg-gray-50 p-10 rounded-md ">
               <div>
-                <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">
+                <h1 className="font-semibold text-gray-700 text-xl md:text-2xl mb-5">
                   Demo API Generator
                 </h1>
-                <p className="font-normal text-gray-500 text-sm md:text-base">
+                <p className="font-normal text-gray-500 ">
                   A dummy data free and documented API generator to facilitate{" "}
                   <br /> the process of testing the front-end portion of
                   projects.
                 </p>
               </div>
             </div>
-            <div className="flex space-x-6">
-              <h1 className="font-normal text-gray-700 text-3xl md:text-4xl">
-                03
-              </h1>
-              <span className="w-28 h-0.5 bg-gray-300 mt-5"></span>
+            <div className="flex space-x-6 bg-gray-50 p-10 rounded-md ">
               <div>
-                <h1 className="font-normal text-gray-700 text-3xl md:text-4xl mb-5">
+                <h1 className="font-semibold text-gray-700 text-xl md:text-2xl mb-5">
                   Demo API Generator
                 </h1>
-                <p className="font-normal text-gray-500 text-sm md:text-base">
+                <p className="font-normal text-gray-500 ">
                   A dummy data free and documented API generator to facilitate{" "}
                   <br /> the process of testing the front-end portion of
                   projects.
