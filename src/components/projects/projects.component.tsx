7a005bafe1d1bca30b499459dc66131ba1990e63
Mikkel Gundersen <301mikkel103@email.com>
2024-07-29 11:05:40 +0200

    fade in thingy

--- src/components/projects/projects.component.tsx
+++ src/components/projects/projects.component.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { useIsVisible } from "@/hooks/use-is-visible";
 import { ProjectsContent } from "@/types/sanity-base";
+import { DetailedHTMLProps, HTMLAttributes, useRef } from "react";
 import { FaExternalLinkAlt, FaLink } from "react-icons/fa";
 
 type Props = {
@@ -21,10 +25,14 @@ export default function Projects({ projectsContents }: Props) {
           <div className="space-y-6 lg:w-5/12">
             {projectsContents.projects &&
               projectsContents.projects.map((project, index) => {
+                var ref = useRef<HTMLDivElement>(null);
+                const isVisible = useIsVisible(ref);
+                console.log(isVisible);
                 return (
                   <div
+                    ref={ref}
                     key={index}
-                    className="flex space-x-6 bg-gray-50  px-8 py-10 rounded-md lg:w-auto "
+                    className={`flex space-x-6 transition-opacity ease-in duration-500 bg-gray-50 px-8 py-10 rounded-md lg:w-auto ${isVisible ? "opacity-100" : "opacity-0"}`}
                   >
                     <div>
                       <h1 className="font-semibold text-gray-700 text-xl md:text-2xl mb-5">
