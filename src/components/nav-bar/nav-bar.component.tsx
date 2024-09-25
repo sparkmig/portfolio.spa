b5a694d7c0fa58f725b9532431293b9995de4823
Mikkel Mckenzie Gundersen <301mikkel103@gmail.com>
2024-09-25 23:25:32 +0200

    fixed spelling error

--- src/components/nav-bar/nav-bar.component.tsx
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -54,11 +54,8 @@ export default function NavBar() {
               className="px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-200 transition ease-linear duration-50"
               href="#Kontakt mig"
             >
-              Kotakt mig
+              Kontakt mig
             </Link>
-            {/* <button className="px-7 py-3 md:px-9 md:py-4 bg-white font-medium md:font-semibold text-gray-700 text-md rounded-md hover:bg-gray-700 hover:text-white transition ease-linear duration-50">
-            Get my CV
-          </button> */}
           </div>
         </nav>
       </div>
